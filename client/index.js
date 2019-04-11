1ca5df7dcbda30dfc7354451d6bbe9b05d54a389
Mykola Maslov <kategorychnost@gmail.com>
2019-04-11 15:25:16 -0400

    git bug

--- client/index.js
+++ client/index.js
@@ -2,22 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
 import {Router} from 'react-router-dom'
-import history from './history'
-import store from './store'
+// import history from './history'
+// import store from './store'
 import App from './app'
 
 //?
 // establishes socket connection
-import './socket'
+// import './socket'
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <App />
-    </Router>
-  </Provider>,
-  document.getElementById('app')
-)
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <Router history={history}>
+//       <App />
+//     </Router>
+//   </Provider>,
+//   document.getElementById('app')
+// )
 
 
 
